0c63e0f66ef2dcd92a5a400f65ba0f6bc97dec70
Brady Murren <bsm4978@ad.rit.edu>
2016-11-11 22:30:34 -0500

    Added basic enemy AI script

---
+++ ImagineCup2016/Assets/Scripts/NavCharScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavCharScript : MonoBehaviour {
+
+    NavMeshAgent agent;
+    GameObject player;
+    bool triggered;
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = 7;
+        agent.acceleration = 10;
+        player = GameObject.Find("Player");
+        triggered = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 playerPos = player.transform.position;
+        if (Vector3.Distance(playerPos, transform.position) < 20)
+        {
+            agent.SetDestination(playerPos);
+        }
+    }
+}
