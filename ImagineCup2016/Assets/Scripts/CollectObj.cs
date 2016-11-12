9d1e04c738031a96328ff8de66499f52713198e7
Sulfur <rc7604@g.rit.edu>
2016-11-12 15:17:10 -0500

    Enemies can wander

---
+++ ImagineCup2016/Assets/Scripts/CollectObj.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectObj : MonoBehaviour {
+    //This script will be attached to the objects that will be collected
+
+    public DoorOpen levelObj;//corresponding to getting to next level
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnTriggerEnter(Collider plyColl)
+    {
+        if(plyColl.tag=="Player")
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+             levelObj.CurrObj += 1;
+             Destroy(this.gameObject);//get rid of object
+            }
+        }
+    }
+}
