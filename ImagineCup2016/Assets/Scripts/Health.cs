bab6b7ffef62d0e3c4ee212e1c102ddd6a10254f
Brady Murren <bsm4978@ad.rit.edu>
2016-11-12 11:25:41 -0500

    Added health and proximity transform scripts.  edited scripts for variable setting in unity UI

---
+++ ImagineCup2016/Assets/Scripts/Health.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+
+    public int health;
+    public int invTime;
+    int timer;
+    bool isDamaged;
+
+	// Use this for initialization
+	void Start () {
+        isDamaged = false;
+        timer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(timer >= invTime && isDamaged)
+        {
+            health--;
+            timer = 0;
+            isDamaged = false;
+        }
+        timer++;
+	}
+
+    public void takeDamage()
+    {
+        isDamaged = true;
+    }
+}
---
+++ ImagineCup2016/Assets/Scripts/ProximityTransform.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProximityTransform : MonoBehaviour {
+
+    public GameObject player;
+    public int proximity;
+    public Material idle;
+    public Material active;
+    Renderer rend;
+
+    // Use this for initialization
+    void Start () {
+        rend = GetComponent<Renderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 playerPos = player.transform.position;
+        if (Vector3.Distance(playerPos, transform.position) < proximity)
+        {
+            rend.material = active;
+        }
+        else
+        {
+            rend.material = idle;
+        }
+    }
+}
